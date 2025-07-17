665fa740f526206bb29074369b0b3484ad2703bf
Gustavo Maciel <gustavomsilva233@gmail.com>
2025-07-17 17:07:03 -0300

    calculadora

--- src/academy/devdojo/maratonajava/introducaometodos/dominio/Calculadora.java
+++ src/academy/devdojo/maratonajava/introducaometodos/dominio/Calculadora.java
@@ -6,21 +6,31 @@ public class Calculadora {
         System.out.println(10 + 10);
     }
 
-    public void subtraiDoisNumeros(){
-        System.out.println(50-27);
+    public void subtraiDoisNumeros() {
+        System.out.println(50 - 27);
     }
 
-    public void multiplicaDoisNumeros(int num, int num2){
+    public void multiplicaDoisNumeros(int num, int num2) {
         System.out.println(num * num2);
     }
+
     public double divideDoisNumeros(double num1, double num2) {
 
-        if (num2 == 0){
+        if (num2 == 0) {
             return 0;
+        } else {
+            return num1 / num2;
         }
-        else
-            return num1/num2;
 
     }
 
+    public void alteraDoisNumeros(int num1, int num2) {
+        num1 = 99;
+        num2 = 100;
+        System.out.println("Dentro do alteraDoisNumeros");
+        System.out.println("num1: " + num1);
+        System.out.println("num2: " + num2);
+
+
+    }
 }
---
+++ src/academy/devdojo/maratonajava/introducaometodos/test/CalculadoraTest04.java
@@ -0,0 +1,12 @@
+package academy.devdojo.maratonajava.introducaometodos.test;
+
+import academy.devdojo.maratonajava.introducaometodos.dominio.Calculadora;
+
+public class CalculadoraTest04 {
+    public static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+        int num1= 1;
+        int num2= 3;
+
+    }
+}
